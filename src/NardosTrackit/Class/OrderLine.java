18c3762f26f3d90a04cc090e6dcf5a6f337491af
Victoria Whyte <vwhyte10@yahoo.com>
2024-11-28 18:04:12 -0500

    initial starter files

---
+++ src/NardosTrackit/Class/OrderLine.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NardosTrackit.Class;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import NardosTrackit.Helpers.*;
+
+/**
+ *
+ * @author Sourcecodester
+ */
+public class OrderLine {
+    ItemService itemService = new ItemService();
+    private String custName;
+    private int orderID;
+    private String name;
+    private int quantity;
+    private double price;
+
+    public OrderLine(){
+        
+    }
+    
+    public OrderLine(String custName, int orderID, String name, int quantity, double price) {
+        this.custName = custName;
+        this.orderID = orderID;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getCustName(){
+        return this.custName;
+    }
+
+    public void setCustName(String custName){
+        this.custName = custName;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public List<Order> getAll() {
+        
+        List<Order> orders = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new FileInputStream("storage/order.txt"))) {
+            while (scanner.hasNextLine()) {
+                String itemLine = scanner.nextLine();
+
+                String itemInfo[] = itemLine.split(",");
+
+                Order order = new Order(Integer.parseInt(itemInfo[0]), Double.parseDouble(itemInfo[1]),itemInfo[2]);
+
+                orders.add(order);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ItemService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return orders;
+    }
+}
---
+++ src/NardosTrackit/Helpers/OrderService.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NardosTrackit.Helpers;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import NardosTrackit.Class.Cart;
+import NardosTrackit.Class.CartItem;
+import NardosTrackit.Class.Order;
+
+/**
+ *
+ * @author Sourcecodester
+ */
+public class OrderService {
+    
+    private Cart cart;
+    
+    public OrderService() {
+        this.cart = new Cart(new ArrayList<>(), 0);
+    }
+    
+    public void addToCart(CartItem cartItem) {
+        this.cart.addItemToCart(cartItem);
+    }
+    
+    public void clearCart() {
+        this.cart = new Cart(new ArrayList<>(), 0);
+    }
+    
+    public Cart getCart() {
+        return this.cart;
+    }
+
+    public List<Order> getAll() {
+        
+        List<Order> orders = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new FileInputStream("storage/order.txt"))) {
+            while (scanner.hasNextLine()) {
+                String itemLine = scanner.nextLine();
+
+                String itemInfo[] = itemLine.split(",");
+
+                Order order = new Order(Integer.parseInt(itemInfo[0]), Double.parseDouble(itemInfo[1]),itemInfo[2]);
+
+                orders.add(order);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ItemService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return orders;
+    }
+}
